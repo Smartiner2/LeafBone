d3213bd824c61f98774b8d393f27c6c097a3e08b
Smartiner2 <smartiner2@eafit.edu.co>
2026-05-27 08:17:05 -0500

    arreglar el salto

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 10f; 
+    private Rigidbody2D rb; 
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+void Start() 
+{ 
+    rb = GetComponent<Rigidbody2D>(); 
+} 
+
+void Update() 
+{ 
+    if (Input.GetButtonDown("Jump") && CheckGround.isGrounded) 
+    { 
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); 
+    } 
+
+}
+
+
+
 }
